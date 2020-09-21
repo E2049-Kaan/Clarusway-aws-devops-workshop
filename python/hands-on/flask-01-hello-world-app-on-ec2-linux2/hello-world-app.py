50794e956a8dee99349b6df8579bfe5f0dd442bf
waltman <harriswalt9@gmail.com>
2020-09-21 19:36:51 +0300

    yeni dosyalar geldi.

--- python/hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
+++ python/hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+<<<<<<< HEAD
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
+=======
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+if __name__=='__main__':
+   app.run(host='0.0.0.0', port=80)
+>>>>>>> 65570f59f5f0b56194343c920fdd13ca9e337090

a941b7ce841dcfc29930d1c3f87fd870359ef891
waltman <harriswalt9@gmail.com>
2020-10-03 14:27:53 +0300

    adding app.py

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 def convert(decimal_num):
     roman = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
     num_to_roman = ''
@@ -10,34 +9,26 @@ def convert(decimal_num):
         num_to_roman += roman[i]*(decimal_num//i)
         decimal_num %= i
     return num_to_roman
-print(convert(3999))
 
-@app.route("/", methods=["GET"])
+@app.route('/', methods=['GET'])
 def main_get():
-    return render_template("index.html", developer_name="Walt", not_valid=False)
+    return render_template('index.html', developer_name='Walt', not_valid=False)
 
-@app.route("/", methods=["POST"])
+@app.route('/', methods=['POST'])
 def main_post():
-    alpha = request.form["number"]
+    alpha = request.form['number']
     if not alpha.isdecimal():
-        return render_template("index.html", developer_name="Walt", not_valid=True)
+        return render_template('index.html', developer_name='Walt', not_valid=True)
 
     number = int(alpha)
     if not 0 < number < 4000:
-        return render_template("index.html", developer_name="Walt", not_valid=True)
+        return render_template('index.html', developer_name='Walt', not_valid=True)
 
-    return render_template("result.html", number_decimal = number , number_roman = convert(number) , developer_name = "Walt")
+    return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Walt')
 
-
-
-
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(host="0.0.0.0", port=80)
-
-
+    app.run(host='0.0.0.0', port=80)
 
 
 
